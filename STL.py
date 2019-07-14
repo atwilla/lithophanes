5d97b2f59968ee57c2aad6df8411eb3ca3df1e28
Alex Twilla <alexandertwilla@gmail.com>
2019-07-14 19:15:37 -0400

    Write code (bugged) to place facets for block bases. Add test files.

--- STL.py
+++ STL.py
@@ -16,21 +16,20 @@ class Facet:
 		else:
 			file = open(outputFile, "a")
 
-		print("\tfacet normal %e %e %e" % (self.normal[0], self.normal[1], self.normal[2]), file=file)
-		print("\t\touter loop", file=file)
-
-		for vertex in self.verteces:
-			print("\t\t\tvertex %e %e %e" % (vertex[0], vertex[1], vertex[2]), file=file)
-
-		print("\t\tendloop", file=file)
-		print("\tend facet", file=file)
-
+		print(self.toStr(), end="", file=file)
 		file.close()
 
-	def toString(self):
+	def toStr(self):
 		"""Construct a string that represents the STL facet."""
 
-		facetStr = "facet normal"
+		facetStr = "\tfacet normal " + "%e %e %e\n\t\touter loop\n" % (self.normal[0], self.normal[1], self.normal[2])
+		
+		for vertex in self.verteces:
+			facetStr += "\t\t\tvertex %e %e %e\n" % (vertex[0], vertex[1], vertex[2])
+
+		facetStr += "\t\tendloop\n\tend facet\n"
+
+		return facetStr
 
 
 class Shape:
@@ -49,22 +48,56 @@ class Shape:
 			print("solid " +  self.name, file=file)
 			print("name written")
 
-		for facet in self.facets_:
-			facet.print(outputFile)
+			for facet in self.facets_:
+				print(facet.toStr(), end="", file=file)
 
-		with open(outputFile, 'a') as file:
 			print("endsolid " + self.name, file=file)
 			print("end written")
 
 
 class Lithopane(Shape):
 
-	def __init__(self, image, desiredWidth):
-		self.image = Image.open(image)
+	def __init__(self, name, image, desiredWidth=40, maxHeight=2):
+		"""
+		Create Lithopane object. All units are millimeters unless specified 
+		otherwise.
+		"""
+
+		Shape.__init__(self, name)
+		self.image = Image.open(image).convert('L')
+		self.pixels = list(self.image.getdata())
+		self.cols = self.image.size[0]
+		self.rows = self.image.size[1]
 		self.width = desiredWidth
+		self.pixelWidth = self.width / self.cols
+		self.height = self.pixelWidth * self.rows
+
+		for row in range(self.rows):
+
+			for col in range(self.cols):
+				currLeftCorner = [(col / self.cols) * self.width, self.height - (self.pixelWidth * row), maxHeight]
+				self.addBlock(currLeftCorner, 0)
+
+	def addBlock(self, topLeft, height):
+		# Create block base
+		topRight = [topLeft[0] + self.pixelWidth, topLeft[1], 0]
+		bottomRight = [topRight[0], topRight[1] - self.pixelWidth, 0]
+		bottomLeft = [topLeft[0], topLeft[1] - self.pixelWidth, 0]
+		baseNormal = [0, 0, -1]
+
+		self.addFacet(Facet([topLeft, topRight, bottomLeft], baseNormal))
+		self.addFacet(Facet([topRight, bottomRight, bottomLeft], baseNormal))
+
+	def getData(self):
+		print("Lithopane Dimensions: ")
+		return (self.width, self.height)
 		
 
-facet = Facet(verteces=[[0, 0, 0], [0, 1, 0], [0, 0, 1]], normal=[0, 0, 1])
-shape = Shape("testSolid")
-shape.addFacet(facet)
-shape.print("test-output.stl")
+# facet = Facet(verteces=[[0, 0, 0], [0, 1, 0], [0, 0, 1]], normal=[0, 0, 1])
+# print(facet.toStr())
+# shape = Shape("testSolid")
+# shape.addFacet(facet)
+# shape.print("test-output.stl")
+litho = Lithopane("test-litho", "test-picture-2.png")
+print(litho.getData())
+litho.print("test-litho.stl")
