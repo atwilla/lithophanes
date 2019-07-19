3a6275124661b34b42831617642e4ee0caed9273
Alex Twilla <alexandertwilla@gmail.com>
2019-07-19 14:50:55 -0400

    Give lithotest ability to manipulate lithopane parameters.

--- lithotest.py
+++ lithotest.py
@@ -1,5 +1,23 @@
 import sys
 import STL
 
-litho = STL.Lithopane("test-litho", sys.argv[1])
-litho.print("test-litho.stl")
+args = sys.argv
+options = {"-o":'litho-output.stl', '-w':'100', '-p':'100000', '-h':'2'}
+
+if len(args) == 1:
+	print("Usage: lithotest.py input")
+	print("Options: ")
+	print("\t-o output\n\t-w width(mm)\n\t-p max pixels\n\t-h max height")
+
+else:
+
+	for flag in options:
+		if flag in args:
+			options[flag] = args[args.index(flag) + 1]
+
+
+	litho = STL.Lithopane(str(options['-o']), args[1], 
+		desiredWidth=int(options['-w']), maxHeight=int(options['-h']), 
+			maxPixels=int(options['-p']))
+
+	litho.print(options['-o'])
